6ef6c53616b9db4a57880b877a84003319c09dd3
gamax <mazuelosrabaza.gabriel@gmail.com>
2019-12-20 18:53:54 -0400

    ui

---
+++ src/Components/NuevaCita.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+
+class NuevaCita extends Component {
+    state = { 
+        cita: {
+            mascota: '',
+            propietario: '',
+            fecha: '',
+            hora: '',
+            sintomas: ''
+        }
+
+     }
+    render() { 
+        return ( 
+            <div className="card mt-5 py-5">
+                <div className="card-body">
+                    <h2 className="card-title text-center mb-5">
+                        Llena el formulario para crear la cita
+                    </h2>
+                    <form>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre mascota"
+                                    name="mascota"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Due;o</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre due;o mascota"
+                                    name="propietario"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+                            <div className="col-sm-8 col-lg-4">
+                                <input
+                                    type="date"
+                                    className="form-control"
+                                    name="fecha"
+                                />
+                            </div>
+
+                            <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+                            <div className="col-sm-8 col-lg-4">
+                                <input
+                                    type="time"
+                                    className="form-control"
+                                    name="hora"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <textarea
+                                    className="form-control"
+                                    name="sintomas"
+                                    placeholder="Describe los sintomas"
+                                    >
+                                </textarea>
+                            </div>
+                        </div>
+                        <input type="submit" className="py-3 mt-2 btn btn-success btn-block"
+                            value="Agregar nueva cita"
+                        />
+
+                    </form>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default NuevaCita;
